5acfdc057b8f4388138db62165c5056009e7dc2b
Jeanette Clark <jclark@nceas.ucsb.edu>
2026-05-08 11:19:57 -0700

    remove login methods

--- src/dataone/adapters/fastapi.py
+++ src/dataone/adapters/fastapi.py
@@ -4,8 +4,3 @@ class FastAPIAuthAdapter(BaseAuthAdapter):
     def _initialize_oauth(self):
         from authlib.integrations.starlette_client import OAuth
         return OAuth()
-
-    async def login(self, name: str, request, **kwargs):
-        client = self.oauth.create_client(name)
-        # FastAPI/Starlette is async and requires the request object
-        return await client.authorize_redirect(request, **kwargs)
--- src/dataone/adapters/flask.py
+++ src/dataone/adapters/flask.py
@@ -4,8 +4,3 @@ class FlaskAuthAdapter(BaseAuthAdapter):
     def _initialize_oauth(self):
         from authlib.integrations.flask_client import OAuth
         return OAuth()
-
-    def login(self, name: str, **kwargs):
-        client = self.oauth.create_client(name)
-        # Standard Flask is synchronous, no request object needed
-        return client.authorize_redirect(**kwargs)
